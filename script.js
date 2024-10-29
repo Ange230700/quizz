4d0d37f0fa4b8310728d0dc5b71350d3aab240a0
Ange230700 <kouakouangeericstephane@gmail.com>
2024-10-29 16:23:52 +0100

    on a géré le clique sur le bouton suivant (en mob programming avec les membres de l'équipe)

--- script.js
+++ script.js
@@ -16,12 +16,15 @@ const boutonSuivantDansHtml = document.querySelector("#bouton-suivant");
 // on récupère la section quiz dans le HTML
 const sectionQuizDansHtml = document.querySelector("#section-quiz");
 
-function chargerLaQuestion() {
-//on vide la section des options dans HTML
-sectionDesOptionsDansHtml.innerHTML = "";
+// on récupère le bouton rejouer dans le HTML
+const boutonRejouerDansHtml = document.querySelector("#bouton-rejouer");
 
-// on récupère la question actuelle
-const questionActuelle = questions[numeroQuestionActuelle];
+function chargerLaQuestion() {
+    //on vide la section des options dans HTML
+    sectionDesOptionsDansHtml.innerHTML = "";
+
+    // on récupère la question actuelle
+    const questionActuelle = questions[numeroQuestionActuelle];
 
     //on injecte la question actuelle dans le HTML
     questionDansHtml.innerText = questionActuelle.texte;
@@ -53,16 +56,43 @@ boutonSuivantDansHtml.addEventListener("click", () => {
         chargerLaQuestion();
     } else {
         // S'il y a plus de questions, on indique que le quiz est terminé
-        sectionQuizDansHtml.innerText = "Quiz terminé !";
+        questionDansHtml.innerText = 'Quiz terminé';
 
         //on vide la section des options dans HTML
         sectionDesOptionsDansHtml.innerHTML = "";
 
-        // on cache le bouton suivant
-        boutonSuivantDansHtml.style.display = "none";
+        // // on cache le bouton suivant
+         boutonSuivantDansHtml.style.display = "none";
+        
     }
 })
 
+// on ajoute un événement click au bouton rejouer
+boutonRejouerDansHtml.addEventListener("click", () => {
+    // On remet le numéro de la question actuelle à 0
+    numeroQuestionActuelle = 0
+
+    // On cache le bouton rejouer
+    boutonRejouerDansHtml.style.display = "none";
+
+    // On affiche le bouton suivant
+    //boutonSuivantDansHtml.style.display = "inline-block";
+
+    // On remet le texte de la section quiz à vide
+    sectionQuizDansHtml.innerHTML = `
+     <h1 class="question"></h1>
+
+      <section class="options"></section>
+      <section class="section-bouton">
+        <button class="bouton-clique-en-bas-a-droite" id="bouton-suivant">Suivant</button>
+        <button class="bouton-clique-en-bas-a-droite" id="bouton-rejouer" style="display: none;">Rejouer</button>
+      </section>
+    `;
+
+    // On charge la question
+    chargerLaQuestion();
+
+})
 // // on récupère la première question dans le tableau de question
 // const premiereQuestion = questions[0];
 
@@ -73,13 +103,13 @@ boutonSuivantDansHtml.addEventListener("click", () => {
 // premiereQuestion.tableauDeChoix.forEach((choix) => {
 //     // on crée un bouton pour chaque option dans le HTML
 //     const boutonDansHtml = document.createElement("button");
-    
+
 //     // on affiche le texte de tableau de choix dans la section de classe options HTML
 //     boutonDansHtml.innerText = choix;
-   
+
 //     // on ajoute une classe option du CSS à chaque bouton
 //     boutonDansHtml.classList.add("option");
-    
+
 //     // on ajoute chaque bouton à la section des options dans le HTML
 //     sectionDesOptionsDansHtml.appendChild(boutonDansHtml);
 // });
