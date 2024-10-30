0e5116ace9eeab234f1eabce6d29af7427b6a972
Ange230700 <kouakouangeericstephane@gmail.com>
2024-10-30 15:19:15 +0100

    on a ajouté un suivi de score

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import questions from './questions.js';
 
 let numeroQuestionActuelle = 0;
 
+let score = 0
+
 /* ======= RECUPERATION ELEMENTS HTML ======= */
 
 // on récupère les éléments HTML pour la question 
@@ -17,14 +19,27 @@ const boutonSuivantDansHtml = document.querySelector("#bouton-suivant");
 // on récupère le bouton rejouer dans le HTML
 const boutonRejouerDansHtml = document.querySelector("#bouton-rejouer");
 
+//on récupère l'element HTML de l'id de score
+const scoreDansHtml = document.querySelector("#score");
+scoreDansHtml.innerText = `Score: ${score} / ${questions.length}`;
+
 /* ==================================== */
 
 /* =============== FONCTIONS =============== */
+
+function desactiverLesBoutonsOptions() {
+    const boutonOptionsDansHtml = document.querySelectorAll(".option");
+
+    boutonOptionsDansHtml.forEach((bouton) => {
+        bouton.disabled = true;
+    });
+}
 
 function verifierBonneReponse(reponseJoueur) {
     const bonneReponseDeQuestionActuelle = questions[numeroQuestionActuelle].reponse;
 
     if (reponseJoueur === bonneReponseDeQuestionActuelle) {
+        score = score + 1;
         return true;
     } else {
         return false
@@ -57,19 +72,21 @@ function chargerLaQuestion() {
         // on ajoute chaque bouton à la section des options dans le HTML
         sectionDesOptionsDansHtml.appendChild(boutonDansHtml);
 
-      
+
         // on ajoute un événement click à chaque bouton
         boutonDansHtml.addEventListener("click", () => {
             desactiverLesBoutonsOptions();
-            if (verifierBonneReponse(boutonDansHtml.innerText) === true ) {
+            if (verifierBonneReponse(boutonDansHtml.innerText) === true) {
                 boutonDansHtml.style.backgroundColor = "green";
                 boutonDansHtml.style.color = "white";
                 boutonSuivantDansHtml.disabled = false;
-                
+                scoreDansHtml.innerText = `Score: ${score} / ${questions.length}`;
+
             } else {
                 boutonDansHtml.style.backgroundColor = "red";
                 boutonDansHtml.style.color = "white";
                 boutonSuivantDansHtml.disabled = false;
+                scoreDansHtml.innerText = `Score: ${score} / ${questions.length}`;
             }
         });
     });
@@ -81,13 +98,21 @@ function chargerLaQuestion() {
 
 chargerLaQuestion();
 
-function desactiverLesBoutonsOptions() {
-    const boutonOptionsDansHtml = document.querySelectorAll(".option");
-
-    boutonOptionsDansHtml.forEach((bouton) => {
-        bouton.disabled = true;
-    });
+function changerLeMessageEnFonctionDuScore() {
+    if (score === questions.length) {
+        questionDansHtml.innerText = "Bravo! T'es un boss des séries!";
+    } else if (score > (3 * questions.length) / 4) {
+        questionDansHtml.innerText = "Excellent ! Tu maîtrises vraiment bien tes séries !";
+    } else if (score > questions.length / 2) {
+        questionDansHtml.innerText = "Pas mal ! Tu connais bien tes séries !";
+    } else if (score > 0) {
+        questionDansHtml.innerText = "Pas encore ça, mais tu as de bonnes bases !";
+    } else {
+        questionDansHtml.innerText = "Tu devrais regarder plus de séries !";
+    }
+    return questionDansHtml.innerText;
 }
+
 
 
 
@@ -99,21 +124,21 @@ function desactiverLesBoutonsOptions() {
 boutonSuivantDansHtml.addEventListener("click", () => {
     // On incrémente le numéro de la question actuelle
     numeroQuestionActuelle = numeroQuestionActuelle + 1;
-    
+
     // On vérifie si il reste des questions
     if (numeroQuestionActuelle < questions.length) {
         // On charge la question
         chargerLaQuestion();
     } else {
         // S'il y a plus de questions, on indique que le quiz est terminé
-        questionDansHtml.innerText = 'Quiz terminé';
-        
+        questionDansHtml.innerText = changerLeMessageEnFonctionDuScore();
+
         //on vide la section des options dans HTML
         sectionDesOptionsDansHtml.innerHTML = "";
-        
+
         // // on cache le bouton suivant
         boutonSuivantDansHtml.style.display = "none";
-        
+
         // on affiche le bouton rejouer
         boutonRejouerDansHtml.style.display = "inline-block";
     }
@@ -121,15 +146,18 @@ boutonSuivantDansHtml.addEventListener("click", () => {
 
 // on ajoute un événement click au bouton rejouer
 boutonRejouerDansHtml.addEventListener("click", () => {
+    // On remet le score à 0
+    score = 0
+    scoreDansHtml.innerText = `Score: ${score} / ${questions.length}`;
     // On remet le numéro de la question actuelle à 0
     numeroQuestionActuelle = 0
-    
+
     // On cache le bouton rejouer
     boutonRejouerDansHtml.style.display = "none";
-    
+
     // On affiche le bouton suivant
     boutonSuivantDansHtml.style.display = "inline-block";
-    
+
     // On charge la question
     chargerLaQuestion();
 })
