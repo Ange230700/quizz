5666212a9c00af1be049da1326c41b4fdabee8db
Ange230700 <kouakouangeericstephane@gmail.com>
2024-10-31 12:10:17 +0100

    j'ai créé l'implémentation dynamique des thématiques et j'ai codé l'interface utilisateur pour que l'utilisateur puisse voir la thématique qu'il a sélectionné

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 
-// on importe le tableau de questions depuis questions.js
+import listeThematiques from './javascript/listeThematiques.js';
 import questionsFilmsEtSeries from './javascript/questionsFilmsEtSeries.js';
 import questionsMusique from "./javascript/questionsMusique.js";
 import questionsTechnologie from "./javascript/questionsTechnologie.js";
@@ -39,7 +39,9 @@ const boutonRejouerDansHtml = document.querySelector("#bouton-rejouer");
 const scoreDansHtml = document.querySelector("#score");
 scoreDansHtml.innerText = `Score: ${score} / ${questionsThematiqueChoisie.length}`;
 
-const boutonsThematiquesDansHtml = document.querySelectorAll("#thematiques button");
+// on récupère la section des boutons thématiques dans le HTML
+const sectionDesBoutonsThematiquesDansHtml = document.querySelector("#thematiques");
+
 
 /* ==================================== */
 
@@ -47,7 +49,7 @@ const boutonsThematiquesDansHtml = document.querySelectorAll("#thematiques butto
 
 function desactiverLesBoutonsOptions() {
     const boutonOptionsDansHtml = document.querySelectorAll(".option");
-
+    
     boutonOptionsDansHtml.forEach((bouton) => {
         bouton.disabled = true;
     });
@@ -55,7 +57,7 @@ function desactiverLesBoutonsOptions() {
 
 function verifierBonneReponse(reponseJoueur) {
     const bonneReponseDeQuestionActuelle = questionsThematiqueChoisie[numeroQuestionActuelle].reponse;
-
+    
     if (reponseJoueur === bonneReponseDeQuestionActuelle) {
         score = score + 1;
         return true;
@@ -64,33 +66,31 @@ function verifierBonneReponse(reponseJoueur) {
     }
 }
 
-
-
 function chargerLaQuestion() {
     //on vide la section des options dans HTML
     sectionDesOptionsDansHtml.innerHTML = "";
-
+    
     // on récupère la question actuelle
     const questionActuelle = questionsThematiqueChoisie[numeroQuestionActuelle];
-
+    
     //on injecte la question actuelle dans le HTML
     questionDansHtml.innerText = questionActuelle.texte;
-
+    
     //on injecte les choix dans le tableau de choix de la question actuelle dans le HTML
     questionActuelle.tableauDeChoix.forEach((choix) => {
         // on crée un bouton pour chaque option dans le HTML
         const boutonDansHtml = document.createElement("button");
-
+        
         // on affiche le texte de tableau de choix dans la section de classe options HTML
         boutonDansHtml.innerText = choix;
-
+        
         // on ajoute une classe option du CSS à chaque bouton
         boutonDansHtml.classList.add("option");
-
+        
         // on ajoute chaque bouton à la section des options dans le HTML
         sectionDesOptionsDansHtml.appendChild(boutonDansHtml);
-
-
+        
+        
         // on ajoute un événement click à chaque bouton
         boutonDansHtml.addEventListener("click", () => {
             desactiverLesBoutonsOptions();
@@ -99,7 +99,7 @@ function chargerLaQuestion() {
                 boutonDansHtml.style.color = "white";
                 boutonSuivantDansHtml.disabled = false;
                 scoreDansHtml.innerText = `Score: ${score} / ${questionsThematiqueChoisie.length}`;
-
+                
             } else {
                 boutonDansHtml.style.backgroundColor = "red";
                 boutonDansHtml.style.color = "white";
@@ -108,78 +108,100 @@ function chargerLaQuestion() {
             }
         });
     });
-
-
+    
+    
     // on désactive le bouton suivant
     boutonSuivantDansHtml.disabled = true;
 }
-
-chargerLaQuestion();
 
 function changerThematique(thematique) {
     switch (thematique) {
         case "films-et-series":
             questionsThematiqueChoisie = questionsFilmsEtSeries;
             break;
-        case "musique":
-            questionsThematiqueChoisie = questionsMusique;
-            break;
-        case "technologie":
-            questionsThematiqueChoisie = questionsTechnologie;
-            break;
-        case "histoire":
-            questionsThematiqueChoisie = questionsHistoire;
-            break;
-        case "citations":
-            questionsThematiqueChoisie = questionsCitations;
-            break;
-        case "marvel":
-            questionsThematiqueChoisie = questionsMarvel;
-            break;
+            case "musique":
+                questionsThematiqueChoisie = questionsMusique;
+                break;
+                case "technologie":
+                    questionsThematiqueChoisie = questionsTechnologie;
+                    break;
+                    case "histoire":
+                        questionsThematiqueChoisie = questionsHistoire;
+                        break;
+                        case "citations":
+                            questionsThematiqueChoisie = questionsCitations;
+                            break;
+                            case "marvel":
+                                questionsThematiqueChoisie = questionsMarvel;
+                                break;
+                            }
+                            
+                            // On remet le numéro de la question actuelle à 0
+                            numeroQuestionActuelle = 0
+                            
+                            // On remet le score à 0
+                            score = 0
+                            scoreDansHtml.innerText = `Score: ${score} / ${questionsThematiqueChoisie.length}`;
+                            
+                            chargerLaQuestion();
+                        }
+                        
+                        function changerLeMessageEnFonctionDuScore() {
+                            if (score === questionsThematiqueChoisie.length) {
+                                questionDansHtml.innerText = "Bravo! T'es un boss des séries!";
+        } else if (score > (3 * questionsThematiqueChoisie.length) / 4) {
+            questionDansHtml.innerText = "Excellent ! Tu maîtrises vraiment bien tes séries !";
+        } else if (score > questionsThematiqueChoisie.length / 2) {
+            questionDansHtml.innerText = "Pas mal ! Tu connais bien tes séries !";
+        } else if (score > 0) {
+            questionDansHtml.innerText = "Pas encore ça, mais tu as de bonnes bases !";
+        } else {
+            questionDansHtml.innerText = "Tu devrais regarder plus de séries !";
+        }
+        return questionDansHtml.innerText;
     }
-
-    // On remet le numéro de la question actuelle à 0
-    numeroQuestionActuelle = 0
-
-    // On remet le score à 0
-    score = 0
-    scoreDansHtml.innerText = `Score: ${score} / ${questionsThematiqueChoisie.length}`;
-
-    chargerLaQuestion();
-}
-
-function changerLeMessageEnFonctionDuScore() {
-    if (score === questionsThematiqueChoisie.length) {
-        questionDansHtml.innerText = "Bravo! T'es un boss des séries!";
-    } else if (score > (3 * questionsThematiqueChoisie.length) / 4) {
-        questionDansHtml.innerText = "Excellent ! Tu maîtrises vraiment bien tes séries !";
-    } else if (score > questionsThematiqueChoisie.length / 2) {
-        questionDansHtml.innerText = "Pas mal ! Tu connais bien tes séries !";
-    } else if (score > 0) {
-        questionDansHtml.innerText = "Pas encore ça, mais tu as de bonnes bases !";
-    } else {
-        questionDansHtml.innerText = "Tu devrais regarder plus de séries !";
-    }
-    return questionDansHtml.innerText;
-}
-
+    
 function activerBoutonThematiqueSelectionne(boutonThematiqueSelectionne) {
-    boutonsThematiquesDansHtml.forEach((bouton) => {
-        bouton.classList.remove("active");
+        // on récupère les boutons thématiques dans le HTML
+        const boutonsThematiquesDansHtml = document.querySelectorAll("#thematiques button");
+        
+        // on parcourt les boutons thématiques dans le HTML
+        boutonsThematiquesDansHtml.forEach((boutonThematique) => {
+            // on enlève la classe active à tous les boutons thématiques
+            boutonThematique.classList.remove("active");
+        });
+
+        // on ajoute la classe active au bouton thématique sélectionné
+        boutonThematiqueSelectionne.classList.add("active");
+    }
+
+    function creerBoutonsThematiquesDansHtml() {
+    listeThematiques.forEach((thematique, index) => {
+        const boutonThematique = document.createElement("button");
+        boutonThematique.setAttribute("data-thematique", thematique.data_thematique);
+        boutonThematique.innerText = thematique.nom;
+        
+        boutonThematique.addEventListener("click", () => {
+            changerThematique(thematique.data_thematique);
+            activerBoutonThematiqueSelectionne(boutonThematique);
+        });
+        
+        sectionDesBoutonsThematiquesDansHtml.appendChild(boutonThematique);
     });
 
-    boutonThematiqueSelectionne.classList.add("active");
+    // on ajoute la classe active au premier bouton thématique
+    const premierBoutonThematiqueDansHtml = document.querySelector("#thematiques button");
+    if (premierBoutonThematiqueDansHtml !== null && premierBoutonThematiqueDansHtml !== undefined) {
+        activerBoutonThematiqueSelectionne(premierBoutonThematiqueDansHtml);
+        changerThematique(premierBoutonThematiqueDansHtml.getAttribute("data-thematique"));
+    }
 }
 
-boutonsThematiquesDansHtml.forEach((bouton) => {
-    const thematique = bouton.getAttribute("data-thematique");
-    if (thematique === "films-et-series") {
-        bouton.classList.add("active");
-    }
-})
-
+creerBoutonsThematiquesDansHtml();
+chargerLaQuestion();
 
 /* ============================================== */
+
 
 /* =============== GESTION DES ÉVÉNEMENTS =============== */
 
@@ -187,7 +209,7 @@ boutonsThematiquesDansHtml.forEach((bouton) => {
 boutonSuivantDansHtml.addEventListener("click", () => {
     // On incrémente le numéro de la question actuelle
     numeroQuestionActuelle = numeroQuestionActuelle + 1;
-
+    
     // On vérifie si il reste des questions
     if (numeroQuestionActuelle < questionsThematiqueChoisie.length) {
         // On charge la question
@@ -195,17 +217,17 @@ boutonSuivantDansHtml.addEventListener("click", () => {
     } else {
         // S'il y a plus de questions, on indique que le quiz est terminé
         questionDansHtml.innerText = changerLeMessageEnFonctionDuScore();
-
+        
         //on vide la section des options dans HTML
         sectionDesOptionsDansHtml.innerHTML = "";
-
+        
         // // on cache le bouton suivant
         boutonSuivantDansHtml.style.display = "none";
-
+        
         // on affiche le bouton rejouer
         boutonRejouerDansHtml.style.display = "inline-block";
     }
-})
+});
 
 // on ajoute un événement click au bouton rejouer
 boutonRejouerDansHtml.addEventListener("click", () => {
@@ -214,24 +236,17 @@ boutonRejouerDansHtml.addEventListener("click", () => {
     scoreDansHtml.innerText = `Score: ${score} / ${questionsThematiqueChoisie.length}`;
     // On remet le numéro de la question actuelle à 0
     numeroQuestionActuelle = 0
-
+    
     // On cache le bouton rejouer
     boutonRejouerDansHtml.style.display = "none";
-
+    
     // On affiche le bouton suivant
     boutonSuivantDansHtml.style.display = "inline-block";
-
+    
     // On charge la question
     chargerLaQuestion();
 })
 
-boutonsThematiquesDansHtml.forEach((bouton) => {
-    bouton.addEventListener("click", () => {
-        activerBoutonThematiqueSelectionne(bouton);
-        const thematique = bouton.getAttribute("data-thematique");
-        changerThematique(thematique);
-    });
-});
 
 
 /* ===================================================== */
